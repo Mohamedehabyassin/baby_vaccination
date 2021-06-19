087825711eb00bc36f56e69aa49962613e0dd522
SaWy <mohamedehabyassin99@gmail.com>
2021-06-19 18:01:49 +0200

    Last update

--- lib/Pages/Home/baby_details.dart
+++ lib/Pages/Home/baby_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vacc_app/Pages/Home/vacc_settings.dart';
 
 class BabyDetails extends StatefulWidget {
   final data;
@@ -10,16 +11,110 @@ class BabyDetails extends StatefulWidget {
 }
 
 class _BabyDetailsState extends State<BabyDetails> {
+  var vaccList = [];
+  int goal = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    modifyVaccList();
+    super.initState();
+  }
+
+  void modifyVaccList() {
+    for (var data in widget.data['babyVaccination']) {
+      setState(() {
+        vaccList.add(VaccSettings(title: data));
+      });
+    }
+  }
+
+  void modifyGoal() {
+    int counter = 0;
+    for (var data in vaccList) {
+      if (data.value == true) {
+        counter++;
+      }
+    }
+    setState(() {
+      if(counter == 0){
+        goal= 0;
+      }
+      else{
+        goal = ((counter / vaccList.length)*100).toInt() ;
+      }
+
+    });
+  }
+
+  Widget buildSingleCheckbox(VaccSettings settings) => buildCheckbox(
+      vaccSettings: settings,
+      onClicked: () {
+        setState(() {
+          final newValue = !settings.value;
+          settings.value = newValue;
+        });
+      });
+
+  Widget buildDivider() => Divider(
+        indent: 60,
+        endIndent: 60,
+        color: Colors.blue[300],
+        thickness: 3,
+        height: 50,
+      );
+
+  Widget buildCheckbox({
+    @required VaccSettings vaccSettings,
+    @required VoidCallback onClicked,
+  }) =>
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(90),
+        ),
+        child: ListTile(
+          onTap: onClicked,
+          leading: Checkbox(
+            value: vaccSettings.value,
+            onChanged: (val) => onClicked(),
+          ),
+          title: Text(
+            '${vaccSettings.title}',
+            style: TextStyle(
+              color: Colors.indigo[900],
+              wordSpacing: 2,
+              fontSize: 18,
+            ),
+          ),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.indigo[900],
         automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: modifyGoal, icon: Icon(Icons.house_siding_outlined))
+        ],
+        title: (goal == null)
+            ? Text(
+                'Goal : ',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              )
+            : Text(
+                'Goal : $goal %',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
       ),
       body: Container(
         margin: EdgeInsets.all(20),
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 30),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
@@ -30,32 +125,18 @@ class _BabyDetailsState extends State<BabyDetails> {
               '${widget.data['babyName'].toString().toUpperCase()}',
               style: TextStyle(color: Colors.white, fontSize: 30),
             ),
-            Divider(
-              indent: 60,
-              endIndent: 60,
-              color: Colors.blue[300],
-              thickness: 5,
-              height: 50,
-            ),
+            buildDivider(),
             Text(
               'Birthdate : ${widget.data['babyBirthday'].toString().toUpperCase()}',
               style: TextStyle(color: Colors.white, fontSize: 20),
             ),
-            Divider(
-              indent: 60,
-              endIndent: 60,
-              color: Colors.blue[300],
-              thickness: 5,
-              height: 50,
-            ),
+            buildDivider(),
             Text(
-              "${widget.data['babyVaccination']}",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                wordSpacing: 2
-              ),
-            )
+              '${widget.data['babyGender']}',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            buildDivider(),
+            ...vaccList.map((e) => buildSingleCheckbox(e)).toList()
           ],
         ),
       ),
---
+++ lib/Pages/Home/vacc_settings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class VaccSettings {
+  String title;
+  bool value;
+
+  VaccSettings({@required this.title,this.value = false});
+}
